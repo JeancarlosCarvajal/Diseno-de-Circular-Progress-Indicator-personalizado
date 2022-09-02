8e4eb7124172eb2c8f96386fbe13fcaea7707fe1
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-09-02 11:42:25 -0400

    Circular progress Indicator Single

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# b_circular_progress
-
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# b_circular_progress 
+<!-- https://en.wikipedia.org/wiki/Markdown -->
+<!-- https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-readmes  -->
+Disenos de Circular Progress Indicator
+## Appearance Application
+ 
+![Appearance 1](appearance/movie-1.gif)
--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:b_circular_progress/main.dart';
 import 'package:flutter/material.dart';
@@ -11,17 +12,65 @@ class CircularProgressPage extends StatefulWidget {
   State<CircularProgressPage> createState() => _CircularProgressPageState();
 }
 
-class _CircularProgressPageState extends State<CircularProgressPage> {
+class _CircularProgressPageState extends State<CircularProgressPage> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  double porcentaje = 0.0;
+  double nuevoPorcentaje = 0.0;
+
+  @override
+  void initState() {
+
+    controller = AnimationController(
+      vsync: this, 
+      duration: const Duration(milliseconds: 800)
+    );
+
+    controller.addListener(() {
+      // print('jean: controller: ${controller.value}');
+      // lerpDouble interpola los porcentajes para medir la relacion de 1 a porcentaje
+      setState(() {
+        porcentaje = lerpDouble(porcentaje, nuevoPorcentaje, controller.value)!;
+      });
+      
+    });
+    
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.pink,
+        onPressed: () {
+          porcentaje = nuevoPorcentaje;
+          nuevoPorcentaje += 5;
+          if(porcentaje > 100) {
+            nuevoPorcentaje = 0;
+            porcentaje = 0;
+          }
+
+          // from: 0.0 es para que al darle forward se empiece desde cero
+          controller.forward(from: 0.0);
+          setState(() {});
+        },
+        child: const Icon(Icons.refresh),
+      ),
       body: Center(
         child: Container(
+          padding: const EdgeInsets.all(5),
           width: 300,
           height: 300,
-          color: Colors.red,
+          // color: Colors.red,
           child: CustomPaint(
-            painter: _MiRadialProgress(),
+            painter: _MiRadialProgress(porcentaje: porcentaje),
           ),
         ),
       ),
@@ -30,19 +79,40 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
 }
 
 class _MiRadialProgress extends CustomPainter {
+
+  final porcentaje;
+
+  _MiRadialProgress({required this.porcentaje});
+
+  // Circulo completado
   @override
   void paint(Canvas canvas, Size size) {
     final paint = new Paint()
-      ..strokeWidth = 10
+      ..strokeWidth = 4
       ..color       = Colors.grey
       ..style       = PaintingStyle.stroke;
     
-    Offset center = Offset(size.width*0.5, size.height/2); 
-    double radius = min(size.width*0.5, size.height*0.5);
+    final Offset center = new Offset(size.width * 0.5, size.height * 0.5); 
+    final double radio = min( size.width * 0.5, size.height * 0.5 );
 
-    canvas.drawCircle(center, radius, paint);
+    canvas.drawCircle(center, radio, paint);
     
+    // Arco
+    final paintArco = new Paint()
+      ..strokeWidth = 10
+      ..color       = Colors.pink
+      ..style       = PaintingStyle.stroke;
 
+    double arcAngle = (2 * pi) * ( porcentaje / 100 ); // (2 * pi) es unna vuelta completa
+
+    // dibujar el arco
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radio), 
+      -pi/2, 
+      arcAngle, 
+      false, 
+      paintArco
+    );
 
   }
 
